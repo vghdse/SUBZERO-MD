e3c2f331fb8ba13c7d1aae4929050f427b87b900
vghdse <h.hanayan66@gmail.com>
2025-06-20 23:22:07 +0200

    Update config.js

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
     // ===== BOT CORE SETTINGS =====
     SESSION_ID: process.env.SESSION_ID || "SUBZERO~45f982ff5cc562ab2cf798004fe53721e5335ac6",  // Your bot's session ID (keep it secure)
-    PREFIX: getConfig("PREFIX") || ",",  // Command prefix (e.g., "., / ! * - +")
+    PREFIX: getConfig("PREFIX") || ".",  // Command prefix (e.g., "., / ! * - +")
     BOT_NAME: process.env.BOT_NAME || getConfig("BOT_NAME") || "SUBZERO-MD",  // Bot's display name
     MODE: getConfig("MODE") || process.env.MODE || "public",        // Bot mode: public/private/group/inbox
     REPO: process.env.REPO || "https://github.com/mrfrankofcc/SUBZERO-MD",  // Bot's GitHub repo
