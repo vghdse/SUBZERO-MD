2b52daae7518a4ec20b44332de2cf9b48e92f92c
vghdse <h.hanayan66@gmail.com>
2025-07-30 17:37:59 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: "SUBZERO~f97cbf34436d37baffda352c80c81b9394ba1a5e", // Put Session Id Here Inside "*"
+  SESSION_ID: "SUBZERO~d3b1c35e3f4199ebb249f29732f23f4ca78061e2", // Put Session Id Here Inside "*"
   OWNER_NUMBER: "263719647303", // Put Owner Number Here 
   PREFIX: "!" // Default Prefix You Can Change To .,*!?+-&%#@
 };
