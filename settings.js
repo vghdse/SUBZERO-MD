cee2f34a58a5ded6a88ec70e7731d57703cad7a6
vghdse <h.hanayan66@gmail.com>
2025-08-09 17:49:48 +0200

    Update settings.js for Trevor-bot

--- settings.js
+++ settings.js
@@ -1,7 +1,5 @@
 module.exports = {
-  SESSION_ID: "SUBZERO~d3b1c35e3f4199ebb249f29732f23f4ca78061e2", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
-  PREFIX: "!" // Default Prefix You Can Change To .,*!?+-&%#@
+  SESSION_ID: "SUBZERO-MD;;;KFlUBbDD#KCmhO4UvOVGp8o8IgtsVnW8dw1W-3gPLW_647fSXmCs",
+  OWNER_NUMBER: "+256773374844", 
+  PREFIX: "."
 };
-
-// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
