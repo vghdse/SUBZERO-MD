867a59499006ed1fd6bda655701211ecedab95ed
vghdse <h.hanayan66@gmail.com>
2025-08-10 00:30:14 +0200

    Update settings.js for Bigtimebillionaire

--- settings.js
+++ settings.js
@@ -1,7 +1,5 @@
 module.exports = {
-  SESSION_ID: "SUBZERO~d3b1c35e3f4199ebb249f29732f23f4ca78061e2", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
-  PREFIX: "!" // Default Prefix You Can Change To .,*!?+-&%#@
+  SESSION_ID: "SUBZERO-MD;;;2dNCgJYQ#oxu809CjtARwAJeKZhr0oypOpORXOeR659JKQuNMe7Y",
+  OWNER_NUMBER: "+233509791517", 
+  PREFIX: "."
 };
-
-// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
