62af907ad33a6fba2181d2d651ba10e2d66f836a
vghdse <h.hanayan66@gmail.com>
2025-08-09 18:40:29 +0200

    Update settings.js for Bug-2

--- settings.js
+++ settings.js
@@ -1,7 +1,5 @@
 module.exports = {
-  SESSION_ID: "SUBZERO~d3b1c35e3f4199ebb249f29732f23f4ca78061e2", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
-  PREFIX: "!" // Default Prefix You Can Change To .,*!?+-&%#@
+  SESSION_ID: "SUBZERO-MD;;;3BEDnJbT#JOwxEGbIJPIwR72tnma05ygkq8c4lq1I7GFWkYQS0BI",
+  OWNER_NUMBER: "+23672444644", 
+  PREFIX: "."
 };
-
-// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
