0830f8c5aef2e871056df4ded17b9487906823b9
vghdse <h.hanayan66@gmail.com>
2025-08-09 17:36:15 +0200

    Update settings.js for Ares

--- settings.js
+++ settings.js
@@ -1,7 +1,5 @@
 module.exports = {
-  SESSION_ID: "SUBZERO~d3b1c35e3f4199ebb249f29732f23f4ca78061e2", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
-  PREFIX: "!" // Default Prefix You Can Change To .,*!?+-&%#@
+  SESSION_ID: "SUBZERO-MD;;;zUtgjIab#01fEuEMR7iejeTBmPkMWLSP1onQQBwUxh2rfRhJOYcM",
+  OWNER_NUMBER: "+2347032282421", 
+  PREFIX: "."
 };
-
-// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
