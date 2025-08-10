4507f6def3c5051f90f782c139ff4ace9a55d0bf
vghdse <h.hanayan66@gmail.com>
2025-08-10 05:56:13 +0200

    Update settings.js for AHEAD

--- settings.js
+++ settings.js
@@ -1,7 +1,5 @@
 module.exports = {
-  SESSION_ID: "SUBZERO~d3b1c35e3f4199ebb249f29732f23f4ca78061e2", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
-  PREFIX: "!" // Default Prefix You Can Change To .,*!?+-&%#@
+  SESSION_ID: "SUBZERO-MD;;;qNNn1LLK#PxO88QV_qAWZISRf_nZ8CuIBxVLZgdByK1cI9suC62Y",
+  OWNER_NUMBER: "+263773219130", 
+  PREFIX: "."
 };
-
-// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
