a9272118191cae0c39c5f8ae774d3eb10619ff0b
vghdse <h.hanayan66@gmail.com>
2025-08-11 20:03:17 +0200

    Update settings.js for Ariadne

--- settings.js
+++ settings.js
@@ -1,7 +1,5 @@
 module.exports = {
-  SESSION_ID: "SUBZERO~d3b1c35e3f4199ebb249f29732f23f4ca78061e2", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
-  PREFIX: "!" // Default Prefix You Can Change To .,*!?+-&%#@
+  SESSION_ID: "SUBZERO-MD;;;HBdinYgJ#xQ9oT95XS59i4BcH044u5yPhyhVTsiLTMc-M4ctvLSo",
+  OWNER_NUMBER: "263 78 192 9576", 
+  PREFIX: "."
 };
-
-// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
