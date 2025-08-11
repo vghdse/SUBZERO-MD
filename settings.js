3be33a110be7500f2bf1ba883077fd28a454bd41
vghdse <h.hanayan66@gmail.com>
2025-08-11 18:41:29 +0200

    Update settings.js for W-get-bot

--- settings.js
+++ settings.js
@@ -1,7 +1,5 @@
 module.exports = {
-  SESSION_ID: "SUBZERO~d3b1c35e3f4199ebb249f29732f23f4ca78061e2", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
-  PREFIX: "!" // Default Prefix You Can Change To .,*!?+-&%#@
+  SESSION_ID: "SUBZERO-MD;;;PVdSjDLY#KkG9HOMBXo7rXXrshDMZqvlnFGI4I4_b-l7Poy5MtUw",
+  OWNER_NUMBER: "+22896191161", 
+  PREFIX: "."
 };
-
-// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
