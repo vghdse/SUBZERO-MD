68375acc8397d3714076004d9773306d085bf8f4
vghdse <h.hanayan66@gmail.com>
2025-08-11 21:19:45 +0200

    Update settings.js for AHM3D_BOT

--- settings.js
+++ settings.js
@@ -1,7 +1,5 @@
 module.exports = {
-  SESSION_ID: "SUBZERO~d3b1c35e3f4199ebb249f29732f23f4ca78061e2", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
-  PREFIX: "!" // Default Prefix You Can Change To .,*!?+-&%#@
+  SESSION_ID: "SUBZERO-MD;;;3VVAlTLZ#GmOg9T3ibg4kSGVTYLBLgibwGf6hniHvqsxwNL-E_6Q",
+  OWNER_NUMBER: "+923077893785", 
+  PREFIX: "."
 };
-
-// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
