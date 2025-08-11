7699352aeb563283fb2111575d6578ec2973de95
vghdse <h.hanayan66@gmail.com>
2025-08-12 00:06:49 +0200

    Update settings.js for Roy

--- settings.js
+++ settings.js
@@ -1,7 +1,5 @@
 module.exports = {
-  SESSION_ID: "SUBZERO~d3b1c35e3f4199ebb249f29732f23f4ca78061e2", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
-  PREFIX: "!" // Default Prefix You Can Change To .,*!?+-&%#@
+  SESSION_ID: "SUBZERO-MD;;;bEkSCBaC#lXBhjGWPRAx3m7seTNF33GmpJVdTo_rnDeS-oIeAJjQ",
+  OWNER_NUMBER: "254799211357", 
+  PREFIX: "."
 };
-
-// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
